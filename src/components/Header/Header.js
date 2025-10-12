491fd4907c08f991f2c90f2c7ca102529b96cc99
Webcraft Innovation <135718962+samranrajput@users.noreply.github.com>
2025-10-13 00:12:49 +0500

    hello

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,64 @@ import React from "react";
 import GradientBlinds from "../GradientBlinds/GradientBlinds";
 import Navbar from "../Navbar/Navbar";
 import BlurText from "../BlurText/BlurText";
+import TextType from "../TextType/TextType";
+import GlassIcons from "../GlassIcons/GlassIcons";
+import ShinyButtonText from "../ShinyButtonText/ShinyButtonText";
 import "./Header.css";
 import headerImage from "../../assets/header-image.png";
+import myResume from "../../assets/docs/my_resume.pdf";
+import {
+  FaGithub,
+  FaInstagram,
+  FaTiktok,
+  FaLinkedinIn,
+  FaWhatsapp,
+  FaFacebookF,
+  FaCode,
+  FaDatabase,
+  FaPenNib,
+} from "react-icons/fa";
+import { FaMobileScreenButton } from "react-icons/fa6";
 
 function Header() {
-  const handleAnimationComplete = () => {
-    console.log("Animation completed!");
-  };
+  const items = [
+    {
+      icon: <FaInstagram />,
+      color: "#00a384",
+      label: "Instagram",
+      link: "https://www.instagram.com/codeverse_studious/",
+    },
+    {
+      icon: <FaTiktok />,
+      color: "#00a384",
+      label: "Tiktok",
+      link: "https://www.tiktok.com/@codeverse_studious",
+    },
+    {
+      icon: <FaLinkedinIn />,
+      color: "#00a384",
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/codeverse-studios/",
+    },
+    {
+      icon: <FaWhatsapp />,
+      color: "#00a384",
+      label: "Whatsapp",
+      link: "https://whatsapp.com/channel/0029VaDYqDWHVvTRrQ5OoN3p",
+    },
+    {
+      icon: <FaFacebookF />,
+      color: "#00a384",
+      label: "Facebook",
+      link: "https://www.facebook.com/codeversestudious",
+    },
+    {
+      icon: <FaGithub />,
+      color: "#00a384",
+      label: "GibHub",
+      link: "https://github.com/samranrajput",
+    },
+  ];
   return (
     <header>
       <div className="container">
@@ -60,7 +111,7 @@ function Header() {
             </svg>
 
             <figure className="svg-img-container">
-              <img src={headerImage} alt="Header Image" />
+              <img src={headerImage} alt="My Frame" />
             </figure>
           </div>
           <div className="shape"></div>
@@ -69,21 +120,27 @@ function Header() {
         <div className="skill-container">
           <div className="ring">
             <div className="skill skill1 lg-text text-center bg-light-mode text-them">
-              <i className="fa-solid fa-code sm-heading"></i>
+              <i className="normal-heading">
+                <FaCode />
+              </i>
               <p className="lg-text">
                 Frontend Web <br />
                 Developer
               </p>
             </div>
             <div className="skill skill2 lg-text text-center bg-light-mode text-them">
-              <i className="fa-solid fa-database sm-heading"></i>
-              <p className="lg-text">
+              <i className="normal-heading">
+                <FaDatabase />
+              </i>
+              <p className="normal-text">
                 Backend Web <br />
                 Developer
               </p>
             </div>
             <div className="skill skill3 lg-text text-center bg-light-mode text-them">
-              <i className="fa-solid fa-mobile-screen-button sm-heading"></i>
+              <i className="normal-heading">
+                <FaMobileScreenButton />
+              </i>
               <p className="lg-text">
                 Mobile Application
                 <br />
@@ -91,7 +148,9 @@ function Header() {
               </p>
             </div>
             <div className="skill skill4 lg-text text-center bg-light-mode text-them">
-              <i className="fa-solid fa-pen-nib sm-heading"></i>
+              <i className="normal-heading">
+                <FaPenNib />
+              </i>
               <p className="lg-text">Graphic Designer</p>
             </div>
           </div>
@@ -122,7 +181,6 @@ function Header() {
               delay={200}
               animateBy="char"
               direction="top"
-              onAnimationComplete={handleAnimationComplete}
               className="text-2xl mb-4"
             />
             <i>
@@ -131,7 +189,6 @@ function Header() {
                 delay={200}
                 animateBy="char"
                 direction="top"
-                onAnimationComplete={handleAnimationComplete}
                 className="text-2xl mb-4"
               />
             </i>
@@ -142,7 +199,6 @@ function Header() {
               delay={200}
               animateBy="char"
               direction="top"
-              onAnimationComplete={handleAnimationComplete}
               className="text-2xl mb-4"
             />
             <i>
@@ -151,13 +207,35 @@ function Header() {
                 delay={200}
                 animateBy="char"
                 direction="top"
-                onAnimationComplete={handleAnimationComplete}
                 className="text-2xl mb-4"
               />
             </i>
           </h1>
-          <p>Modern Design powered by React & WebGL ✨</p>
-          <button onClick={() => alert("Button clicked!")}>Click Me</button>
+          <p className="lg-text paragraph">
+            <TextType
+              text={[
+                "I'm a Full Stack Web Developer who loves building websites that don't just look great they perform beautifully.",
+                "From dynamic business websites to full-scale web applications, I turn ideas into reality with clean code and modern design.",
+                "My goal is to help businesses grow online through fast, secure, and engaging digital experiences.",
+                "If you're looking for a developer who truly cares about quality, you're in the right place.",
+              ]}
+              typingSpeed={30}
+              pauseDuration={5000}
+              showCursor={true}
+              cursorCharacter="_"
+            />
+          </p>
+          <div className="icon-container">
+            <GlassIcons items={items} className="custom-class" />
+          </div>
+          <div className="btn-container">
+            <a href={myResume} target="_blank" rel="noreferrer">
+              <ShinyButtonText text="My Resume" speed={2.5} />
+            </a>
+            <a href="https://wa.me/923172960156" target="_blank" rel="noreferrer">
+              <ShinyButtonText text="Hire Me Now !" speed={2.5} />
+            </a>
+          </div>
         </div>
       </div>
     </header>
---
+++ src/components/ShinyButtonText/ShinyButtonText.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ShinyButtonText.css";
+
+const ShinyButtonText = ({ text = "Shiny Button", speed = 3, disabled = false, className = "" }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <button
+      className={`shiny-button-text ${disabled ? "disabled" : ""} ${className}`}
+      style={{ "--shine-speed": animationDuration }}
+      disabled={disabled}
+    >
+      <span className="shine-text lg-text">{text}</span>
+    </button>
+  );
+};
+
+export default ShinyButtonText;
